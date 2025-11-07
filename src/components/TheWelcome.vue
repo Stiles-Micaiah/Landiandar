dd6d42b765b49606861d71ed9ad269381b02a2f0
stiles-micaiah <syluxprime40@gmail.com>
2025-11-07 17:43:52 -0600

    initial commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,10 @@
+<template>
+<div class="card text-bg-dark">
+  <img src="..." class="card-img" alt="...">
+  <div class="card-img-overlay">
+    <h5 class="card-title">Welcome to my Page</h5>
+    <p class="card-text">This is my internal landing page. If you are here by mistake, please use the navigation bar to select the intended page. I may or may not have the search function working. Try it out, but beware kinks</p>
+    <!-- <p class="card-text"><small>Last updated 3 mins ago</small></p> -->
+  </div>
+</div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import NavBar from '@/components/NavBar.vue';
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    
+  </main>
+</template>
